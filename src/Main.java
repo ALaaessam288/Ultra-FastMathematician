3aab1c48e25711fdf595f4702a2214ec1642c922
Alaa Harb <alaaessam288@gmail.com>
2023-07-29 08:42:22 +0300

    codeForces

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner =new Scanner(System.in) ;
+
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine() ;
+        StringBuilder result = new StringBuilder();
+        for (int i=0; i<s1.length() ;i++)
+        {
+            if (s1.charAt(i) !=s2.charAt(i))
+            {
+                result.append('1') ;
+            }
+            else
+                result.append('0') ;
+        }
+        System.out.println(result);
+
+
+
     }
 }
